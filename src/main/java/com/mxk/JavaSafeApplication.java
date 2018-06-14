bb6ca2672eb7b05df302eed1b015935f3065245a
mengxk <1005682757@qq.com>
2018-06-14 20:40:31 +0800

    add

---
+++ src/main/java/com/mxk/JavaSafeApplication.java
@@ -0,0 +1,12 @@
+package com.mxk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSafeApplication.class, args);
+	}
+}
