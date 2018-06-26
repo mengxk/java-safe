0306440cb3efb3269afc3cdb15365acb51a97e8c
mengxk <1005682757@qq.com>
2018-06-26 19:24:03 +0800

    DES

--- src/main/java/com/mxk/utils/DESUtil.java
+++ src/main/java/com/mxk/utils/DESUtil.java
@@ -1,22 +1,52 @@
 package com.mxk.utils;
 
+import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+
+import static com.mxk.utils.BASE64Util.base642byte;
+
 
 /**
  * Created by mengxk on 2018/6/14.
  */
 public class DESUtil {
+    /**
+     * 生成DES密钥
+     * @return
+     * @throws NoSuchAlgorithmException
+     */
     public static String getKeyDES() throws NoSuchAlgorithmException {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
         keyGenerator.init(56);
         SecretKey secretKey = keyGenerator.generateKey();
-        String base64Str = byte2base64(secretKey.getEncoded());
+        String base64Str = BASE64Util.byte2base64(secretKey.getEncoded());
         return base64Str;
     }
 
-    private static String byte2base64(byte[] encoded) {
-        return "";
+    public static SecretKey loadKeyDES(String base64Key) throws IOException {
+        byte[] bytes = base642byte(base64Key);
+        SecretKey secretKey = new SecretKeySpec(bytes, "DES");
+        return secretKey;
     }
+
+    /**
+     * 加密
+     * @param source
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public static byte[] encryptDES(byte[] source, SecretKey key) throws Exception{
+        Cipher cipher = Cipher.getInstance("DES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] bytes = cipher.doFinal(source);
+        return bytes;
+    }
+
+    
+
 }
---
+++ src/test/java/com/mxk/utils/DESUtilTest.java
@@ -0,0 +1,15 @@
+package com.mxk.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mengxk on 2018/6/26.
+ */
+public class DESUtilTest {
+    @Test
+    public void encryptDES() throws Exception {
+    }
+
+}
